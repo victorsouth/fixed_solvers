f38ff976b972af62aaa8fbd0e252254b82f6a1c0
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2023-03-13 13:44:28 +0300

    test: Линейные ограничения, Ньютон-Рафсон, СЛАУ

--- fixed/fixed.h
+++ fixed/fixed.h
@@ -3,6 +3,18 @@
 #ifndef __FIXED_H__
 #define __FIXED_H__
 
+/// @brief Возвращает знак числа
+/// @return для отрицательных -1, для положительных +1
+template <typename T> inline int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+/// @brief Возведение вещественной переменной в квадрат
+inline double sqr(double x) {
+    return x * x;
+}
+
+
 #include "fixed/array_ext.h"
 #include "fixed/fixed_system.h"
 #include "fixed/fixed_linear_solver.h"
---
+++ testing/test_linear_system.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+/// @brief Тест решения линейной системы
+TEST(Common, LinearEquationSolver)
+{
+    array<array<double, 2>, 2> A;
+    A[0] = { 2, 2 };
+    A[1] = { 3, 4 };
+
+    array<double, 2> b = { 6, 11 };
+
+
+    auto x = solve_linear_system(A, b);
+}
