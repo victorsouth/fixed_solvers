b74c109362b561423ac960dbe3213c50d54dbc0e
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2023-04-13 21:26:28 +0300

    feat: custom_success_criteria

--- fixed/fixed_nonlinear_solver.h
+++ fixed/fixed_nonlinear_solver.h
@@ -375,7 +375,7 @@ struct fixed_solver_parameters_t
 
 enum class numerical_result_code_t
 {
-    NoNumericalError, IllConditionedMatrix, LargeConditionNumber,
+    NoNumericalError, IllConditionedMatrix, LargeConditionNumber, CustomCriteriaFailed,
     NotConverged, NumericalNanValues, LineSearchFailed, Converged
 };
 
@@ -465,13 +465,13 @@ struct fixed_solver_result_t {
     /// @brief Завершение итерационной процедуры
     numerical_result_code_t result_code{ numerical_result_code_t::NotConverged };
     /// @brief Балл сходимости
-    convergence_score_t score;
+    convergence_score_t score{convergence_score_t::Poor};
     /// @brief Остаточная невязка по окончании численного метода
-    function_type residuals;
+    function_type residuals{ fixed_system_types<Dimension>::default_var() };
     /// @brief Норма остаточной невязки по окончании численного метода
-    double residuals_norm;
+    double residuals_norm{0};
     /// @brief Искомый аргумент по окончании численного метода
-    var_type argument;
+    var_type argument{ fixed_system_types<Dimension>::default_var()};
     /// @brief Количество выполненных итераций
     size_t iteration_count{ 0 };
 };
@@ -736,6 +736,11 @@ public:
                 result->score = convergence_score_t::Poor;
             }
         }
+        if (!residuals.custom_success_criteria(r, argument)) {
+            result->result_code = numerical_result_code_t::CustomCriteriaFailed;
+            result->score = convergence_score_t::Poor;
+        }
+
     }
 
 };
