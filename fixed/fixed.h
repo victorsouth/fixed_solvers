70a28c712c696cc03738eda364f2607b414cbbd8
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2025-12-30 03:14:52 +0300

    build: Должна взлететь сборка под GNU

--- fixed/fixed.h
+++ fixed/fixed.h
@@ -87,6 +87,42 @@ inline std::string wide2string(const std::wstring& str)
         [](wchar_t s) { return static_cast<char>(s); });
     return ascii_string;
 }
+
+/// from https://stackoverflow.com/questions/148403/utf8-to-from-wide-char-conversion-in-stl/148665#148665
+inline std::wstring UTF8_to_wchar(const char* in)
+{
+    std::wstring out;
+    unsigned int codepoint;
+    while (*in != 0)
+    {
+        unsigned char ch = static_cast<unsigned char>(*in);
+        if (ch <= 0x7f)
+            codepoint = ch;
+        else if (ch <= 0xbf)
+            codepoint = (ch << 6) | (ch & 0x3f);
+        else if (ch <= 0xdf)
+            codepoint = ch & 0x1f;
+        else if (ch <= 0xef)
+            codepoint = ch & 0x0f;
+        else
+            codepoint = ch & 0x07;
+        ++in;
+        if (((*in & 0xc0) != 0x80) && (codepoint <= 0x10ffff))
+        {
+            if constexpr (sizeof(wchar_t) > 2)
+                out.append(1, static_cast<wchar_t>(codepoint));
+            else if (codepoint > 0xffff)
+            {
+                out.append(1, static_cast<wchar_t>(0xd800 + (codepoint >> 10)));
+                out.append(1, static_cast<wchar_t>(0xdc00 + (codepoint & 0x03ff)));
+            }
+            else if (codepoint < 0xd800 || codepoint >= 0xe000)
+                out.append(1, static_cast<wchar_t>(codepoint));
+        }
+    }
+    return out;
+}
+
 inline std::wstring string2wide(const std::string& str)
 {
 #ifndef __GNUC__
